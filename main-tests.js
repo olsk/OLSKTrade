d66d8e2d10f9d46d380b420eea59e89cd8de46d4
Rosano Coutinho <pub@xeari.com>
2020-09-23 11:44:06 -0300

    Extract OLSKTrade

---
+++ main-tests.js
@@ -0,0 +1,465 @@
+const { throws, deepEqual } = require('assert');
+
+const mod = require('./main.js');
+
+const liveEnabled = false;
+
+describe('OLSKTradeStripeSession', function test_OLSKTradeStripeSession() {
+
+	const uStripe = function () {
+		return {
+			checkout: {
+				sessions: {
+					retrieve () {
+						return Array.from(arguments);
+					},
+				},
+			},
+		};
+	};
+
+	it('throws if param1 not stripe', function () {
+		throws(function () {
+			mod.OLSKTradeStripeSession(null, '');
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if param2 not string', function () {
+		throws(function () {
+			mod.OLSKTradeStripeSession(uStripe, null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns checkout.sessions.retrieve', function () {
+		const item = Date.now().toString();
+		deepEqual(mod.OLSKTradeStripeSession(uStripe, item), [item, {
+			expand: ['customer'],
+		}]);
+	});
+
+	if (liveEnabled) {
+		it('returns live data', async function () {
+			deepEqual(JSON.stringify(await mod.OLSKTradeStripeSession(require('stripe'), 'cs_test_349RhhKHLSqyTxTGDF7zoZvLEm8eEss0snlu991ZOm7c5FvDVubbvFFo')), '');
+		});
+	}
+
+});
+
+describe('OLSKTradeStripeSubscription', function test_OLSKTradeStripeSubscription() {
+
+	const uStripe = function () {
+		return {
+			subscriptions: {
+				retrieve () {
+					return Array.from(arguments);
+				},
+			},
+		};
+	};
+
+	it('throws if param1 not stripe', function () {
+		throws(function () {
+			mod.OLSKTradeStripeSubscription(null, '');
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if param2 not string', function () {
+		throws(function () {
+			mod.OLSKTradeStripeSubscription(uStripe, null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns subscriptions.retrieve', function () {
+		const item = Date.now().toString();
+		deepEqual(mod.OLSKTradeStripeSubscription(uStripe, item), [item]);
+	});
+
+	if (liveEnabled) {
+		it('returns live data', async function () {
+			deepEqual(JSON.stringify(await mod.OLSKTradeStripeSubscription(require('stripe'), 'sub_HxEfQdyTuBzal0')), '');
+		});
+	}
+
+});
+
+describe('OLSKTradeStripeInvoice', function test_OLSKTradeStripeInvoice() {
+
+	const uStripe = function () {
+		return {
+			invoices: {
+				retrieve () {
+					return Array.from(arguments);
+				},
+			},
+		};
+	};
+
+	it('throws if param1 not stripe', function () {
+		throws(function () {
+			mod.OLSKTradeStripeInvoice(null, '');
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if param2 not string', function () {
+		throws(function () {
+			mod.OLSKTradeStripeInvoice(uStripe, null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns invoices.retrieve', function () {
+		const item = Date.now().toString();
+		deepEqual(mod.OLSKTradeStripeInvoice(uStripe, item), [item]);
+	});
+
+	if (liveEnabled) {
+		it('returns live data', async function () {
+			deepEqual(JSON.stringify(await mod.OLSKTradeStripeInvoice(require('stripe'), 'in_0HNKC3YiCgQEaSkygSOetZ6p')), '');
+		});
+	}
+
+});
+
+describe('OLSKTradeStripeCharge', function test_OLSKTradeStripeCharge() {
+
+	const uStripe = function () {
+		return {
+			charges: {
+				retrieve () {
+					return Array.from(arguments);
+				},
+			},
+		};
+	};
+
+	it('throws if param1 not stripe', function () {
+		throws(function () {
+			mod.OLSKTradeStripeCharge(null, '');
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if param2 not string', function () {
+		throws(function () {
+			mod.OLSKTradeStripeCharge(uStripe, null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns charges.retrieve', function () {
+		const item = Date.now().toString();
+		deepEqual(mod.OLSKTradeStripeCharge(uStripe, item), [item]);
+	});
+
+	if (liveEnabled) {
+		it('returns live data', async function () {
+			deepEqual(JSON.stringify(await mod.OLSKTradeStripeCharge(require('stripe'), 'ch_0HNKC4YiCgQEaSkyywcDEnVg')), '');
+		});
+	}
+
+});
+
+describe('OLSKTradeStripeListCharges', function test_OLSKTradeStripeListCharges() {
+
+	const uStripe = function () {
+		return {
+			charges: {
+				list () {
+					return {
+						data: [{
+							alfa: 'bravo',
+						}],
+					};
+				},
+			},
+		};
+	};
+
+	it('throws if param1 not stripe', function () {
+		throws(function () {
+			mod.OLSKTradeStripeListCharges(null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns charges.list', function () {
+		deepEqual(mod.OLSKTradeStripeListCharges(uStripe), [{
+			alfa: 'bravo',
+		}]);
+	});
+
+	if (liveEnabled) {
+		it('returns live data', async function () {
+			deepEqual((await mod.OLSKTradeStripeListCharges(require('stripe'))), [{}]);
+		});
+	}
+
+});
+
+describe('OLSKTradeStripeListInvoices', function test_OLSKTradeStripeListInvoices() {
+
+	const uStripe = function () {
+		return {
+			invoices: {
+				list () {
+					return {
+						data: [{
+							alfa: 'bravo',
+						}],
+					};
+				},
+			},
+		};
+	};
+
+	it('throws if not stripe', function () {
+		throws(function () {
+			mod.OLSKTradeStripeListInvoices(null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns invoices.list.data', function () {
+		deepEqual(mod.OLSKTradeStripeListInvoices(uStripe), [{
+			alfa: 'bravo',
+		}]);
+	});
+
+	if (liveEnabled) {
+		it('returns live data', async function () {
+			deepEqual(JSON.stringify(await mod.OLSKTradeStripeListInvoices(require('stripe'))), '');
+		});
+	}
+
+});
+
+describe('OLSKTradeStripeInvoicePrefix', function test_OLSKTradeStripeInvoicePrefix() {
+
+	it('throws if not string', function () {
+		throws(function () {
+			mod.OLSKTradeStripeInvoicePrefix(null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns inputData', function () {
+		deepEqual(mod.OLSKTradeStripeInvoicePrefix(''), '');
+	});
+
+	it('trims over eight characters', function () {
+		deepEqual(mod.OLSKTradeStripeInvoicePrefix('alfabravo'), 'alfabrav');
+	});
+
+});
+
+describe('OLSKTradePayPalAccessToken', function test_OLSKTradePayPalAccessToken() {
+
+	const uPayPal = function (inputData) {
+		return {
+			access: {
+				token () {
+					return {
+						access_token: 'alfa',
+					};
+				},
+			},
+		};
+	};
+
+	it('throws if inputData not paypal', function () {
+		throws(function () {
+			mod.OLSKTradePayPalAccessToken(null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns access.token', function () {
+		deepEqual(mod.OLSKTradePayPalAccessToken(uPayPal), 'alfa');
+	});
+
+	if (liveEnabled) {
+		it('returns live data', async function () {
+			deepEqual((await mod.OLSKTradePayPalAccessToken(mod.DataFoilPayPal)), '');
+		});
+	}
+
+});
+
+describe('OLSKTradePayPalSubscription', function test_OLSKTradePayPalSubscription() {
+
+	const uPayPal = function (inputData) {
+		return {
+			subscriptions: {
+				retrieve () {
+					return Array.from(arguments);
+				},
+			},
+		};
+	};
+
+	it('throws if param1 not paypal', function () {
+		throws(function () {
+			mod.OLSKTradePayPalSubscription(null, '');
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if param2 not string', function () {
+		throws(function () {
+			mod.OLSKTradePayPalSubscription(uPayPal, null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns subscriptions.retrieve', function () {
+		const item = Date.now().toString();
+		deepEqual(mod.OLSKTradePayPalSubscription(uPayPal, item), [item]);
+	});
+
+	if (liveEnabled) {
+		it('returns live data', async function () {
+			deepEqual(JSON.stringify(await mod.OLSKTradePayPalSubscription(mod.DataFoilPayPal, 'I-SRVSXYP043JX')), '');
+		});
+	}
+
+});
+
+describe('OLSKTradePayPalTransactions', function test_OLSKTradePayPalTransactions() {
+
+	const uPayPal = function (inputData) {
+		return {
+			transactions: {
+				list () {
+					return {
+						transaction_details: [{
+							alfa: 'bravo',
+						}],
+					};
+				},
+			},
+		};
+	};
+
+	it('throws if inputData not paypal', function () {
+		throws(function () {
+			mod.OLSKTradePayPalTransactions(null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns transactions.list', function () {
+		deepEqual(mod.OLSKTradePayPalTransactions(uPayPal), [{
+			alfa: 'bravo',
+		}]);
+	});
+
+	if (liveEnabled) {
+		it('returns live data', async function () {
+			deepEqual(JSON.stringify(await mod.OLSKTradePayPalTransactions(mod.DataFoilPayPal)), '');
+		});
+	}
+
+});
+
+describe('OLSKTradePayPalOrder', function test_OLSKTradePayPalOrder() {
+
+	const uPayPal = function (inputData) {
+		return {
+			orders: {
+				retrieve () {
+					return Array.from(arguments);
+				},
+			},
+		};
+	};
+
+	it('throws if param1 not paypal', function () {
+		throws(function () {
+			mod.OLSKTradePayPalOrder(null, '');
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if param2 not string', function () {
+		throws(function () {
+			mod.OLSKTradePayPalOrder(uPayPal, null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns orders.retrieve', function () {
+		const item = Date.now().toString();
+		deepEqual(mod.OLSKTradePayPalOrder(uPayPal, item), [item]);
+	});
+
+	if (liveEnabled) {
+		it('returns live data', async function () {
+			deepEqual(JSON.stringify(await mod.OLSKTradePayPalOrder(mod.DataFoilPayPal, '1JF658855X7653252')), '');
+		});
+	}
+
+});
+
+describe('OLSKTradePayPalPlan', function test_OLSKTradePayPalPlan() {
+
+	const uPayPal = function (inputData) {
+		return {
+			plans: {
+				retrieve () {
+					return Array.from(arguments);
+				},
+			},
+		};
+	};
+
+	it('throws if param1 not paypal', function () {
+		throws(function () {
+			mod.OLSKTradePayPalPlan(null, '');
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if param2 not string', function () {
+		throws(function () {
+			mod.OLSKTradePayPalPlan(uPayPal, null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns plans.retrieve', function () {
+		const item = Date.now().toString();
+		deepEqual(mod.OLSKTradePayPalPlan(uPayPal, item), [item]);
+	});
+
+	if (liveEnabled) {
+		it('returns live data', async function () {
+			deepEqual(JSON.stringify(await mod.OLSKTradePayPalPlan(mod.DataFoilPayPal, 'P-5DR07778R1859725TLWN3OYA')), '');
+		});
+	}
+
+});
+
+describe('OLSKTradePayPalSubscriptionTransactions', function test_OLSKTradePayPalSubscriptionTransactions() {
+
+	const uPayPal = function (inputData) {
+		return {
+			subscriptions: {
+				transactions () {
+					return {
+						transactions: Array.from(arguments),
+					};
+				},
+			},
+		};
+	};
+
+	it('throws if param1 not paypal', function () {
+		throws(function () {
+			mod.OLSKTradePayPalSubscriptionTransactions(null, '');
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if param2 not string', function () {
+		throws(function () {
+			mod.OLSKTradePayPalSubscriptionTransactions(uPayPal, null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns subscriptions.transactions', function () {
+		const item = Date.now().toString();
+		deepEqual(mod.OLSKTradePayPalSubscriptionTransactions(uPayPal, item), [item]);
+	});
+
+	if (liveEnabled) {
+		it('returns live data', async function () {
+			deepEqual(JSON.stringify(await mod.OLSKTradePayPalSubscriptionTransactions(mod.DataFoilPayPal, 'I-SRVSXYP043JX')), '');
+		});
+	}
+
+});
---
+++ main.js
@@ -0,0 +1,171 @@
+const uPromise = function (inputData) {
+	if (inputData instanceof Promise) {
+		return inputData;
+	}
+
+	return {
+		then (res) {
+			return res(inputData);
+		},
+	};
+};
+
+const mod = {
+
+	OLSKTradeStripeListCharges (inputData) {
+		if (typeof inputData !== 'function') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		return uPromise(inputData(process.env.OLSK_DONATE_STRIPE_SECRET_API_KEY).charges.list({
+			limit: 30,
+		})).then(function (inputData) {
+			return inputData.data;
+		});
+	},
+
+	OLSKTradeStripeListInvoices (inputData) {
+		if (typeof inputData !== 'function') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		return uPromise(inputData(process.env.OLSK_DONATE_STRIPE_SECRET_API_KEY).invoices.list({
+			limit: 30,
+		})).then(function (inputData) {
+			return inputData.data;
+		});
+	},
+
+	OLSKTradeStripeSession (param1, param2) {
+		if (typeof param1 !== 'function') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof param2 !== 'string') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		return param1(process.env.OLSK_DONATE_STRIPE_SECRET_API_KEY).checkout.sessions.retrieve(param2, {
+			expand: ['customer'],
+		});
+
+		return param1(process.env.OLSK_DONATE_STRIPE_SECRET_API_KEY).checkout.sessions.retrieve(param2);
+	},
+
+	OLSKTradeStripeSubscription (param1, param2) {
+		if (typeof param1 !== 'function') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof param2 !== 'string') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		return param1(process.env.OLSK_DONATE_STRIPE_SECRET_API_KEY).subscriptions.retrieve(param2);
+	},
+
+	OLSKTradeStripeInvoice (param1, param2) {
+		if (typeof param1 !== 'function') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof param2 !== 'string') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		return param1(process.env.OLSK_DONATE_STRIPE_SECRET_API_KEY).invoices.retrieve(param2);
+	},
+
+	OLSKTradeStripeCharge (param1, param2) {
+		if (typeof param1 !== 'function') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof param2 !== 'string') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		return param1(process.env.OLSK_DONATE_STRIPE_SECRET_API_KEY).charges.retrieve(param2);
+	},
+
+	OLSKTradeStripeInvoicePrefix (inputData) {
+		if (typeof inputData !== 'string') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		return inputData.slice(0, 8);
+	},
+
+	OLSKTradePayPalAccessToken (inputData) {
+		if (typeof inputData !== 'function') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		return uPromise(inputData(process.env.OLSK_DONATE_PAYPAL_CLIENT_ID, process.env.OLSK_DONATE_PAYPAL_CLIENT_SECRET).access.token()).then(function (inputData) {
+			return inputData.access_token;
+		});
+	},
+
+	OLSKTradePayPalSubscription (param1, param2) {
+		if (typeof param1 !== 'function') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof param2 !== 'string') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		return param1(process.env.OLSK_DONATE_PAYPAL_CLIENT_ID, process.env.OLSK_DONATE_PAYPAL_CLIENT_SECRET, process.env.OLSK_DONATE_PAYPAL_TEMP_TOKEN).subscriptions.retrieve(param2);
+	},
+
+	OLSKTradePayPalTransactions (inputData) {
+		if (typeof inputData !== 'function') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		return uPromise(inputData(process.env.OLSK_DONATE_PAYPAL_CLIENT_ID, process.env.OLSK_DONATE_PAYPAL_CLIENT_SECRET, process.env.OLSK_DONATE_PAYPAL_TEMP_TOKEN).transactions.list()).then(function (inputData) {
+			return inputData.transaction_details;
+		});
+	},
+
+	OLSKTradePayPalOrder (param1, param2) {
+		if (typeof param1 !== 'function') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof param2 !== 'string') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		return param1(process.env.OLSK_DONATE_PAYPAL_CLIENT_ID, process.env.OLSK_DONATE_PAYPAL_CLIENT_SECRET, process.env.OLSK_DONATE_PAYPAL_TEMP_TOKEN).orders.retrieve(param2);
+	},
+
+	OLSKTradePayPalPlan (param1, param2) {
+		if (typeof param1 !== 'function') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof param2 !== 'string') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		return param1(process.env.OLSK_DONATE_PAYPAL_CLIENT_ID, process.env.OLSK_DONATE_PAYPAL_CLIENT_SECRET, process.env.OLSK_DONATE_PAYPAL_TEMP_TOKEN).plans.retrieve(param2);
+	},
+
+	OLSKTradePayPalSubscriptionTransactions (param1, param2) {
+		if (typeof param1 !== 'function') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof param2 !== 'string') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		return uPromise(param1(process.env.OLSK_DONATE_PAYPAL_CLIENT_ID, process.env.OLSK_DONATE_PAYPAL_CLIENT_SECRET, process.env.OLSK_DONATE_PAYPAL_TEMP_TOKEN).subscriptions.transactions(param2)).then(function (inputData) {
+			return inputData.transactions;
+		});
+	},
+
+};
+
+Object.assign(exports, mod);
